be4f80fd5f041de9135564e2e820a70d107b321d
harshithatnbr <harshithatnbr@gmail.com>
2021-08-13 11:33:02 +0530

    made changes

---
+++ task1/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$('.home-slider').owlCarousel({
+    items:1,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:7000,
+    loop:true
+});
+
+});
+
+ 
+ 
+    
+lightGallery(document.querySelector('.gallery-container'));
